af796b0dee59675956523aa63a6a3f0cb027be34
AdwinTS <145273482+AdwinTS@users.noreply.github.com>
2024-08-26 13:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('easter-egg-trigger').addEventListener('click', function() {
+    alert('Easter Egg Activated! 🥚');
+   
+});
+console.log("easter egg activated in the left side of the screen in that kamala harris side part of the screen");
